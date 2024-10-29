7e174673cd35faf286d2db0a6bc3cf30738ac916
AmeenFadel <ameenfadel820@gmail.com>
2024-10-29 11:47:39 +0300

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,10 @@ function fun(res) {
     for (let index = res.length - 4; index < res.length; index++) {
         div.innerHTML += `
         <div class="card card1 p-3 m-3 " style="width: 18rem;">
+            <div class="img-box overflow-hidden position-relative"  data-work="Add To Cart">
             <img src="${res[index].simple_thumb}" class="card-img-top" alt="...">
+            </div>
+            
             <div class="card-body">
             <p class="card-text">${res[index].title}</p>
             <h5 class="card-title text-black-50">${res[index].author}</h5>
@@ -17,7 +20,10 @@ function fun(res) {
     for (let index = 8; index < res.length - 30; index++) {
         section5.innerHTML += `
         <div class="card card1 p-3 m-3 " style="width: 18rem;">
+            <div class="img-box overflow-hidden position-relative"  data-work="Add To Cart">
             <img src="${res[index].simple_thumb}" class="card-img-top" alt="...">
+            </div>
+            
             <div class="card-body">
             <p class="card-text">${res[index].title}</p>
             <h5 class="card-title text-black-50">${res[index].author}</h5>
@@ -32,9 +38,9 @@ let section = document.getElementById('section4')
 function fun2(result) {
     section.innerHTML = `
         <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-end">
-                    <div class="imgs w-75 h-auto my-5 p-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-lg-6 col-md-12 col-sm-12 ">
+                    <div class="imgs w-75 ms-5 h-auto my-5 p-3">
                         <img class="w-100" src="${result.cover}" alt="">
                     </div>
                 </div>
@@ -50,17 +56,17 @@ function fun2(result) {
             </div>
         </div>`
 }
-function setActive(button) {
-    const activeButton = document.querySelector('.active-btn');
-    if (activeButton) {
-        activeButton.classList.remove('active-btn');
-    }
-    button.classList.add('active-btn');
-}
 function setActiveNav(button) {
     const activeButtonNav = document.querySelector('.active');
     if (activeButtonNav) {
         activeButtonNav.classList.remove('active');
     }
     button.classList.add('active');
+}
+function setActive(button) {
+    const activeButton = document.querySelector('.active-btn');
+    if (activeButton) {
+        activeButton.classList.remove('active-btn');
+    }
+    button.classList.add('active-btn');
 }
